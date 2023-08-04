2481e39dbde402a2b53b878f804273d98879147a
Altino Leandro Rodiguês <136701250+AltinoLeandroRodrigues@users.noreply.github.com>
2023-08-04 11:10:09 +0100

    css

--- responsividade.js
+++ responsividade.js
@@ -1,22 +1,13 @@
  
 
-function clicar(){
-    let pagina = document.getElementById('navList')
-    let btn = document.getElementById('button')
-    let btn1 = document.getElementById('button1')
+function clicar() {
+    let menuMobile = document.querySelector('.nav-list');
+     
 
-    pagina.style.transform="translateX(0)"
-    btn.style.display="none";
-    btn1.style.display="flex";
-    
-}
-function fechar(){
-    let pagina = document.getElementById('navList')
-    let btn = document.getElementById('button')
-    let btn1 = document.getElementById('button1')
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open')
 
-    pagina.style.transform="translateX(100%)"
-    btn.style.display="block";
-    btn1.style.display="none";
-    
+    } else {
+        menuMobile.classList.add('open')
+    }
 }
